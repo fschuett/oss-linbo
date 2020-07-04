d19e9944526340e9a562f9f91590458851f0dc4c
fschuett <fschuett@gymnasium-himmelsthuer.de>
2020-07-04 10:57:47 +0200

    add wlan address to registrierungsdialog

--- linbo_gui-2.0/registrierungsdialog.cpp
+++ linbo_gui-2.0/registrierungsdialog.cpp
@@ -13,6 +13,8 @@ RegistrierungsDialog::RegistrierungsDialog(QWidget *parent) :
     ui->setupUi(this);
     ui->ipAddress->setValidator(new IP4Validator(this));
     ui->ipAddress->setInputMask("000.000.000.000");
+    ui->ipAddress2->setValidator(new IP4Validator(this));
+    ui->ipAddress2->setInputMask("000.000.000.000");
 }
 
 RegistrierungsDialog::~RegistrierungsDialog()
@@ -21,16 +23,22 @@ RegistrierungsDialog::~RegistrierungsDialog()
 }
 
 RegistrierungsDialog::RegistrierungsDialog(  QWidget* parent, QString& roomName, QString& clientName,
-                                     QString& ipAddress, QString& clientGroup) :
+                                     QString& macAddress, QString& ipAddress,
+                                     QString& macAddress2, QString& ipAddress2, QString& clientGroup) :
     QDialog(parent), ui(new Ui::RegistrierungsDialog)
 {
   ui->setupUi(this);
   ui->ipAddress->setValidator(new IP4Validator(this));
   ui->ipAddress->setInputMask("000.000.000.000");
+  ui->ipAddress2->setValidator(new IP4Validator(this));
+  ui->ipAddress2->setInputMask("000.000.000.000");
 
   ui->roomName->setText(roomName);
   ui->clientName->setText(clientName);
+  ui->macAddress->setText(macAddress);
   ui->ipAddress->setText(ipAddress);
+  ui->macAddress2->setText(macAddress2);
+  ui->ipAddress2->setText(ipAddress2);
   ui->clientGroup->setText(clientGroup);
 
   Qt::WindowFlags flags;
@@ -44,12 +52,14 @@ void RegistrierungsDialog::accept()
     QString *roomName = new QString(ui->roomName->text());
     QString *clientName = new QString(ui->clientName->text());
     QString *ipAddress = new QString(ui->ipAddress->text());
+    QString *ipAddress2 = new QString(ui->ipAddress2->text());
     QString *clientGroup = new QString(ui->clientGroup->text());
 
-    emit(finished(*roomName, *clientName, *ipAddress, *clientGroup));
+    emit(finished(*roomName, *clientName, *ipAddress, *ipAddress2, *clientGroup));
     delete roomName;
     delete clientName;
     delete ipAddress;
+    delete ipAddress2;
     delete clientGroup;
     close();
 }
